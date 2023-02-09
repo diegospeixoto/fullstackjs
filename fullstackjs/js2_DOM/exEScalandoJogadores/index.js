1c0857b361eeb26ed07a2a262c2929682cefa4c6
diegospeixoto <digopeixoto_2002@hotmail.com>
2023-02-09 19:50:45 -0300

    Acrescenta a função remove()

--- fullstackjs/js2_DOM/exEScalandoJogadores/index.js
+++ fullstackjs/js2_DOM/exEScalandoJogadores/index.js
@@ -1,9 +1,31 @@
 function add() { 
+    const inputNome = document.getElementById("nome")
+
     const nomeJogador = document.getElementById("nome").value
     const posicaoJogador = document.getElementById("posicao").value
     const numeroJogador = document.getElementById("numero") .value
 
-    const escalacao = document.getElementById('jogadores')
+    let escalacao = document.getElementById('jogadores')
 
-    escalacao.innerHTML = `<p>${nomeJogador}</p>`
+    let ul = document.createElement('ul')
+    
+    let liNome = document.createElement('li')
+    liNome.innerText = `Nome: ${nomeJogador}`
+
+    let liPosicao = document.createElement('li')
+    liPosicao.innerText = `Posição: ${posicaoJogador}`
+
+    let liNumero = document.createElement('li')
+    liNumero = `Número: ${numeroJogador}`
+    
+    ul.append(liNome, liPosicao, liNumero,)
+    escalacao.append(ul)
+}
+
+function remove() {
+    let escalacao = document.getElementById('jogadores')
+
+    let uls = document.getElementsByTagName('ul')
+
+    escalacao.removeChild(uls[uls.length - 1])
 }
