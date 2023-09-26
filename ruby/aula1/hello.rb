3a7485c777c48e2f0adc1bbacf442bff18be4c41
diegospeixoto <digopeixoto_2002@hotmail.com>
2023-09-26 16:15:31 -0300

    adicionando e organizando aulas de ruby

--- ruby/aula1/hello.rb
+++ ruby/aula1/hello.rb
@@ -1,2 +1,1 @@
 puts 'Hello, world!'
-puts 'Curso de Ruby do OnebitCode'
---
+++ ruby/aula3/unless.rb
@@ -0,0 +1,9 @@
+producto_status = 'closed'
+
+unless producto_status == 'open'
+    check_change = 'can'
+else
+    check_change = 'can not'
+end
+
+puts "You #{check_change} change the product"
